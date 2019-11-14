5a4665a8f255ad16c67104d8773aedd74505a4cd
Zak Saylors <zsaylors11@gmail.com>
2019-11-14 14:16:26 -0700

    Stub Boot/MVC project linke JPA project.

---
+++ UpStream/src/test/java/com/skilldistillery/upstream/UpStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.upstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
