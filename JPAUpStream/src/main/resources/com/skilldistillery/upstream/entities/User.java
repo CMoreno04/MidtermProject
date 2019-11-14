28c0bf16abbf728e37d33bc08921586fe9ae1b4f
Zak Saylors <zsaylors11@gmail.com>
2019-11-14 14:06:17 -0700

    Stub JPA project and user entity.

---
+++ JPAUpStream/src/main/resources/com/skilldistillery/upstream/entities/User.java
@@ -0,0 +1,57 @@
+package com.skilldistillery.upstream.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private boolean active;
+	
+	public User() {}
+	
+	public User(int id, String username, String password, boolean active) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.active = active;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", active=" + active + "]";
+	}
+}
