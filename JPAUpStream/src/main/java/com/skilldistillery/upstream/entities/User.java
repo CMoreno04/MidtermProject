9f4fc1732b73b13c575c22e2112f6a2847bb6676
CMoreno04 <cesar.jos.em26@icloud.com>
2019-11-15 15:45:54 -0700

    maps content to genre and content to media

--- JPAUpStream/src/main/java/com/skilldistillery/upstream/entities/User.java
+++ JPAUpStream/src/main/java/com/skilldistillery/upstream/entities/User.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.upstream.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+@Entity
 public class User {
 	// F I E L D S
 	
