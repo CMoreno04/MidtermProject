90754f4ceb025e9729a4c4717a8bbe3f5afe0085
CMoreno04 <cesar.jos.em26@icloud.com>
2019-11-15 10:32:21 -0700

    removes DAO methods

--- UpStream/src/main/java/com/skilldistillery/upstream/data/UpStreamDAOImpl.java
+++ UpStream/src/main/java/com/skilldistillery/upstream/data/UpStreamDAOImpl.java
@@ -1,48 +1,7 @@
 package com.skilldistillery.upstream.data;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import com.skilldistillery.upstream.entities.RatingReview;
-import com.skilldistillery.upstream.entities.Service;
-import com.skilldistillery.upstream.entities.User;
-
 public class UpStreamDAOImpl implements UpStreamDAO{
 	
-	@PersistenceContext
-	private EntityManager em;
-	
-	
-	@Override
-	public List<Service> getAllServices() {
-		String jpql = "SELECT s FROM Service s ";
-				
-		return em.createQuery(jpql,Service.class).getResultList();
-		
-	}
 
-	@Override
-	public void getContentByService() {
-		
-	}
-
-	@Override
-	public List<RatingReview> getReviews() {
-		
-		return null;
-	}
-
-	@Override
-	public void getCustomers() {
-		
-	}
-
-	@Override
-	public List<User> getUsers() {
-		
-		return null;
-	}
 
 }
