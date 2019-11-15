5482b4efb58a93170fe76a6783c5e431f330dd95
CMoreno04 <cesar.jos.em26@icloud.com>
2019-11-15 14:27:55 -0700

    updates service entity and creates JUNIT test

---
+++ JPAUpStream/src/main/java/com/skilldistillery/upstream/entities/Content.java
@@ -0,0 +1,140 @@
+package com.skilldistillery.upstream.entities;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+public class Content {
+	// F I E L D S
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String title;
+	
+	@Column
+	private String description;
+	
+//	@Column
+//	private List<Genre> genre;
+	
+	@Column(name="review_id")
+	private int reviewId;
+	
+	@Column(name="service_id")
+	private int serviceId;
+	
+	@Column(name="image_id")
+	private int imageId;
+	
+	@Column(name="video_id")
+	private int videoId;
+	
+//	@ManyToMany
+//	@JoinTable(name="service_content",
+//		joinColumns=@JoinColumn(name="service_id"),
+//		inverseJoinColumns=@JoinColumn(name="content_id")
+//	)
+	private List<Service> services;
+	
+	@ManyToMany(mappedBy="contents")
+	private List<User> users;
+	
+//	@ManyToMany
+//	@JoinTable(name="content_genre",
+//		joinColumns=@JoinColumn(name="content_id"),
+//		inverseJoinColumns=@JoinColumn(name="id")
+//	)
+	private List<Genre> genres;
+	
+	@ManyToMany
+	@JoinTable(name="rating_review",
+		joinColumns=@JoinColumn(name="content_id"),
+		inverseJoinColumns=@JoinColumn(name="user_id")
+	)
+	private List<RatingReview> reviews;
+	
+//	private List<Service> services;
+//	
+//	private List<User> users;
+
+ 
+	// C O N S T R U C T O R S
+	
+
+	public Content() {
+		super();
+	}
+
+	// G E T T E R S   A N D   S E T T E R S
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String name) {
+		this.title = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+//	public List<Genre> getGenre() {
+//		return genre;
+//	}
+//
+//	public void setGenre(List<Genre> genre) {
+//		this.genre = genre;
+//	}
+
+	public int getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public int getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(int serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	
+
+	
+
+
+	// H A S H   A N D   E Q A L S
+
+
+	
+
+	// T O  S T R I N G
+	
+
+	
+}
---
+++ JPAUpStream/src/main/java/com/skilldistillery/upstream/entities/User.java
@@ -0,0 +1,144 @@
+package com.skilldistillery.upstream.entities;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+public class User {
+	// F I E L D S
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private boolean admin;
+	private boolean active;
+	private String username;
+	private String password;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Transient
+	@Column(name = "image_id")
+	private String imageId;
+	
+
+//	@Column(name = "content_id")
+//	private List<Service> contentId;  // import when created
+//	private List<Content> favorites;  // import when created
+//	private List<Content> wishlist;  // import when created
+
+//	@ManyToMany
+//	@JoinTable(name="user_content",
+//		joinColumns=@JoinColumn(name="user_id"),
+//		inverseJoinColumns=@JoinColumn(name="content_id")
+//	)
+//	private List <Content> contents;
+	
+	// C O N S T R U C T O R S
+	public User() {}
+
+	public User(int id, boolean admin, boolean active, String username, String password, String firstName, String lastName,
+		String imageId) {
+	super();
+	this.id = id;
+	this.admin = admin;
+	this.active = active;
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.imageId = imageId;
+	}
+
+	// G E T T E R S   A N D   S E T T E R S
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getImageId() {
+		return imageId;
+	}
+
+
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+	}
+
+//
+//	public List<Service> getContentId() {
+//		return contentId;
+//	}
+//
+//	public void setContentId(List<Service> contentId) {
+//		this.contentId = contentId;
+//	}
+//
+//	// T O   S T R I N G
+//	@Override
+//	public String toString() {
+//		return "User [id=" + id + ", admin=" + admin + ", active=" + active + ", username=" + username + ", password="
+//				+ password + ", firstName=" + firstName + ", lastName=" + lastName + ", imageId=" + imageId
+//				+ ", contentId=" + contentId + "]";
+//	}
+
+	
+	// H A S H   A N D   E Q U A L S
+}
