4fdb87e2236592d4459eccdabc1a179460b9240a
Zak Saylors <zsaylors11@gmail.com>
2019-11-15 10:19:20 -0700

    Updated user and RatingReview entities and test cases.

--- JPAUpStream/src/main/resources/com/skilldistillery/upstream/entities/User.java
+++ JPAUpStream/src/main/resources/com/skilldistillery/upstream/entities/User.java
@@ -25,33 +25,28 @@ public class User {
 	private String lastName;
 	@Column(name = "image_id")
 	private String imageId;
-	@Column(name = "service_id")
-	private List<Service> serviceId;  // import when created
-	@Column(name = "serv_total")
-	private int serviceTotal;
-	private List<Content> favorites;  // import when created
-	private List<Content> wishlist;  // import when created
+	@Column(name = "content_id")
+	private List<Service> contentId;  // import when created
+//	private List<Content> favorites;  // import when created
+//	private List<Content> wishlist;  // import when created
 
 	
 	// C O N S T R U C T O R S
 	public User() {}
 
-	public User(int id, boolean admin, boolean active, String username, String password, String firstName,
-			String lastName, String imageId, List<Service> serviceId, int serviceTotal, List<Content> favorites,
-			List<Content> wishlist) {
-		super();
-		this.id = id;
-		this.admin = admin;
-		this.active = active;
-		this.username = username;
-		this.password = password;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.imageId = imageId;
-		this.serviceId = serviceId;
-		this.serviceTotal = serviceTotal;
-		this.favorites = favorites;
-		this.wishlist = wishlist;
+
+	public User(int id, boolean admin, boolean active, String username, String password, String firstName, String lastName,
+		String imageId, List<Service> contentId) {
+	super();
+	this.id = id;
+	this.admin = admin;
+	this.active = active;
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.imageId = imageId;
+	this.contentId = contentId;
 	}
 
 	// G E T T E R S   A N D   S E T T E R S
@@ -75,21 +70,17 @@ public class User {
 		return active;
 	}
 
-
 	public void setActive(boolean active) {
 		this.active = active;
 	}
-
 
 	public String getUsername() {
 		return username;
 	}
 
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 
 	public String getPassword() {
 		return password;
@@ -105,21 +96,17 @@ public class User {
 		return firstName;
 	}
 
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 
 	public String getImageId() {
 		return imageId;
@@ -131,52 +118,23 @@ public class User {
 	}
 
 
-	public List<Service> getServiceId() {
-		return serviceId;
+	public List<Service> getContentId() {
+		return contentId;
 	}
 
-
-	public void setServiceId(List<Service> serviceId) {
-		this.serviceId = serviceId;
+	public void setContentId(List<Service> contentId) {
+		this.contentId = contentId;
 	}
 
-
-	public int getServiceTotal() {
-		return serviceTotal;
-	}
-
-
-	public void setServiceTotal(int serviceTotal) {
-		this.serviceTotal = serviceTotal;
-	}
-
-
-	public List<Content> getFavorites() {
-		return favorites;
-	}
-
-
-	public void setFavorites(List<Content> favorites) {
-		this.favorites = favorites;
-	}
-
-
-	public List<Content> getWishlist() {
-		return wishlist;
-	}
-
-
-	public void setWishlist(List<Content> wishlist) {
-		this.wishlist = wishlist;
-	}
 
 	// T O   S T R I N G
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", admin=" + admin + ", active=" + active + ", username=" + username + ", password="
 				+ password + ", firstName=" + firstName + ", lastName=" + lastName + ", imageId=" + imageId
-				+ ", serviceTotal=" + serviceTotal + "]";
+				+ ", contentId=" + contentId + "]";
 	}
 
+	
 	// H A S H   A N D   E Q U A L S
 }
