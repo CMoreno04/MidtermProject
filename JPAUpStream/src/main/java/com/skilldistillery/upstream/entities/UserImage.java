47110d6d0c4d3c4b20fc3f1a770f68f94490e33d
CMoreno04 <cesar.jos.em26@icloud.com>
2019-11-15 17:46:55 -0700

    creates userImage junit test, all green

--- JPAUpStream/src/main/java/com/skilldistillery/upstream/entities/UserImage.java
+++ JPAUpStream/src/main/java/com/skilldistillery/upstream/entities/UserImage.java
@@ -1,13 +1,10 @@
 package com.skilldistillery.upstream.entities;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity(name = "user_image")
 public class UserImage {
@@ -19,19 +16,15 @@ public class UserImage {
 	@Column(name = "url")
 	private String url;
 
-	@OneToMany(mappedBy="userImage")
-	private List<User> contenImages;
-
 	
 	public UserImage() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public UserImage(String url, List<User> contenImages) {
+	public UserImage(String url) {
 		super();
 		this.url = url;
-		this.contenImages = contenImages;
 	}
 
 	public int getId() {
@@ -50,24 +43,16 @@ public class UserImage {
 		this.url = url;
 	}
 
-	public List<User> getContenImages() {
-		return contenImages;
-	}
-
-	public void setContenImages(List<User> contenImages) {
-		this.contenImages = contenImages;
-	}
-
+	
 	@Override
 	public String toString() {
-		return "UserImage [id=" + id + ", url=" + url + ", contenImages=" + contenImages + "]";
+		return "UserImage [id=" + id + ", url=" + url + ", contenImages=" + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((contenImages == null) ? 0 : contenImages.hashCode());
 		result = prime * result + id;
 		result = prime * result + ((url == null) ? 0 : url.hashCode());
 		return result;
@@ -82,11 +67,6 @@ public class UserImage {
 		if (getClass() != obj.getClass())
 			return false;
 		UserImage other = (UserImage) obj;
-		if (contenImages == null) {
-			if (other.contenImages != null)
-				return false;
-		} else if (!contenImages.equals(other.contenImages))
-			return false;
 		if (id != other.id)
 			return false;
 		if (url == null) {
